35cca6f3b3739b8895f50dedf2f2b39d396c3fdd
KarinaVillarroel <karinamamani834@gmail.com>
2024-01-16 09:07:36 -0400

    subi1

--- mi-segundo-proyecto/src/components/Boton.js
+++ mi-segundo-proyecto/src/components/Boton.js
@@ -1,10 +1,13 @@
 import React from "react";
  
+function Boton({ texto, esBotonClick, funcionClick }) {
 
-function Boton(texto,esBotonclick,funcionclick){
-   return(
-    <button className="esBotonclick">
-        
-    </button>
-   );
+    return (
+        <button className={esBotonClick ? "boton-click" : "boton-reinciar"}
+            onClick={funcionClick}>
+            {texto}
+        </button>
+    )
 }
+
+export default Boton;
