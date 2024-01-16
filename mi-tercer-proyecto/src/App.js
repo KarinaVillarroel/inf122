ed68c213285bf3c8c74b0fc969e3ba3d53568ef3
KarinaVillarroel <karinamamani834@gmail.com>
2024-01-16 11:53:06 -0400

    gmfdkg

--- mi-tercer-proyecto/src/App.js
+++ mi-tercer-proyecto/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Tablero from './componets/Tablero';
+import juego from './components/juego';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {/* <Tablero />
+      <TableroB />
+      <TableroC /> */}
+      <Juego />
+
     </div>
   );
 }
